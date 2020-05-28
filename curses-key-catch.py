9582096b2028f8256d722c1fff194fd6bd325fd9
Chad Larson <techchad2@gmail.com>
2020-05-28 06:50:56 -0500

    Add option to save code to file

--- curses-key-catch.py
+++ curses-key-catch.py
@@ -1,12 +1,24 @@
 # curses key catch
+import argparse
 import curses
 
+FILENAME = "CursesKeyCatchLog.txt"
 
-def curses_main(screen):
+
+def log_to_file(ch: str) -> None:
+    with open(FILENAME, "a") as f:
+        f.write(f"{chr(ch)}  {str(ch)}\n")
+
+
+def curses_main(screen, args):
     screen.addstr(0, 0, "enter 'Q' to quit")
     while True:
         screen.move(1, 0)
         ch  = screen.getch()
+
+        if args.log:
+            log_to_file(ch)
+
         screen.clear()
         screen.addstr(0, 0, "enter 'Q' to quit")
         screen.addstr(1, 0, f"{chr(ch)}   {str(ch)}")
@@ -16,9 +28,12 @@ def curses_main(screen):
 
 
 def main():
-    curses.wrapper(curses_main)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", dest="log", action="store_true",
+                        help="save key codes to file")
+    args = parser.parse_args()
+    curses.wrapper(curses_main, args)
 
 
 if __name__ == "__main__":
     exit(main())
-
